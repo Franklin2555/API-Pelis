ce74363e061d85aa357e2b4a04b033caa1e8dcfa
Kito25 <franklinantonio.cortez@gmail.com>
2021-11-01 09:36:41 -0600

    Practica-Clase

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,24 @@
+import { Component,  OnInit } from '@angular/core';
+import { Pelicula, RespuestaMDB} from '../interfaces/interface'
+import { MoviesService } from '../services/movies.service';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page implements OnInit{
+
+  peliculasRecientes: Pelicula[]=[];
+
+  constructor(private moviesService: MoviesService) {}
+
+  ngOnInit(){
+    this.moviesService.getFeatures()
+    .subscribe((resp: RespuestaMDB)=>{
+      console.log('Respuesta', resp);
+      this.peliculasRecientes = resp.results;
+    });
+  }
+  
+}
